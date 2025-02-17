91e6875c85ffaa1e43da42b09b1792aecbea0ae8
Dendre1997@ <denyskichuk97@gmail.com>
2025-02-17 14:08:29 -0700

    components check

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,11 @@
-
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+    <Header/>
+      <h1>Home</h1>
+      <Footer/>
+    </>
       );
 }
